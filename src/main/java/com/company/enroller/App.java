b213626d3fab2b411823c662840fe95f174aed42
basj2 <bajanik@student.agh.edu.pl>
2020-04-19 11:26:04 +0200

    Step 2 done

--- src/main/java/com/company/enroller/App.java
+++ src/main/java/com/company/enroller/App.java
@@ -1,5 +1,7 @@
 package com.company.enroller;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,8 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+        //SpringApplication app = new SpringApplication(App.class);
+        //app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+        //app.run(args); 
     }
 }
